444709f3fd2dea92d1041cfaf5ee23f36ef408c3
Jean-Sebastien <calestar@gmail.com>
2012-11-03 13:45:55 -0400

    Initial import
    Signed-off-by: Jean-Sebastien <calestar@gmail.com>

---
+++ awesome/menu.lua
@@ -0,0 +1,7 @@
+
+module("my.menu")
+
+My_menu = {
+	{"Chrome","chromium-browser"},
+	{"Rhythmbox","/usr/bin/rhythmbox","/usr/share/pixmaps/rhythmbox-small.xpm"}
+}
---
+++ install.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+ln --symbolic `pwd`/awesome ~/.config/awesome
