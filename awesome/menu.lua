45c91ae46fc04b1d51cb1bc9bd130b6cdd604733
Jean-Sebastien <calestar@gmail.com>
2013-01-30 00:37:55 -0500

    Use google chrome instead of chromium

--- awesome/menu.lua
+++ awesome/menu.lua
@@ -2,6 +2,6 @@
 module("my.menu")
 
 My_menu = {
-	{"Chrome","chromium-browser"},
+	{"Chrome","/usr/bin/google-chrome", "/opt/google/chrome/product_logo_32.png"},
 	{"Rhythmbox","/usr/bin/rhythmbox","/usr/share/pixmaps/rhythmbox-small.xpm"}
 }
