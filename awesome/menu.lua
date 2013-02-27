3eaca98a609dbe0abe38a247bee68849ac5cf421
Jean-Sebastien <calestar@gmail.com>
2013-02-26 22:26:53 -0500

    Add chromium to awesome menu

--- awesome/menu.lua
+++ awesome/menu.lua
@@ -3,5 +3,6 @@ module("my.menu")
 
 My_menu = {
 	{"Chrome","/usr/bin/google-chrome", "/opt/google/chrome/product_logo_32.png"},
+	{"Chromium","/usr/bin/chromium-browser"},
 	{"Rhythmbox","/usr/bin/rhythmbox","/usr/share/pixmaps/rhythmbox-small.xpm"}
 }
